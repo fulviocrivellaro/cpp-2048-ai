266224653d12ffd787334a7c13ff8e2300a1ef21
buccia85@gmail.com <buccia85@gmail.com@94d6117c-1b27-d5c4-1503-c26876d1a635>
2014-06-01 17:00:38 +0000

    Added WorstCaseNextMoveChooser

---
+++ 2048.AI/INextMoveChooser.h
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+
+#ifndef __I_NEXT_MOVE_CHOOSER__
+#define __I_NEXT_MOVE_CHOOSER__
+
+#include "2048Core.h"
+#include "AiMathQuery.h"
+
+namespace AiMath2048
+{
+	namespace Query
+	{
+		class INextMoveChooser
+		{
+		public:
+			virtual ~INextMoveChooser() {}
+			virtual Core2048::Direction getNextMove(const Core2048::TILE grid[SIZE][SIZE]) const = 0;
+		};
+	}
+}
+#endif
