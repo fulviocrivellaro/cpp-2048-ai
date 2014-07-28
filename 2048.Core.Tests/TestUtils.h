9e57e9fe1b9221a88165839555134c8c4b7c8377
buccia85@gmail.com <buccia85@gmail.com@94d6117c-1b27-d5c4-1503-c26876d1a635>
2014-07-28 20:46:16 +0000

    Migrated QueryTest.cpp

--- 2048.Core.Tests/TestUtils.h
+++ 2048.Core.Tests/TestUtils.h
@@ -9,68 +9,18 @@ using namespace MilCore;
 
 namespace MilGridTests
 {
-	void runOnTiles(std::function<void (tilePtr c, tilePtr r)> func)
-	{
-		for (tilePtr c=0; c<SIZE; ++c)
-		{
-			for (tilePtr r=0; r<SIZE; ++r)
-			{
-				func(c, r);
-			}
-		}
-	}
+	void runOnTiles(std::function<void (tilePtr c, tilePtr r)> func);
 
-	void copy(MilGrid& grid, TILE values[SIZE][SIZE])
-	{
-		runOnTiles([&](tilePtr c, tilePtr r)
-		{
-			grid[c][r] = values[c][r];
-		});
-	}
+	void copy(MilGrid& grid, TILE values[SIZE][SIZE]);
 
-	MilGrid getEmptyGrid()
-	{
-		MilGrid result;
-		result.resetGrid();
-		return result;
-	}
+	MilGrid getEmptyGrid();
 
-	MilGrid getFilledGrid(TILE values[SIZE][SIZE])
-	{
-		MilGrid grid;
-		copy(grid, values);
-		return grid;
-	}
+	MilGrid getFilledGrid(TILE values[SIZE][SIZE]);
 
-	MilGrid getFilledGrid()
-	{
-		TILE values[SIZE][SIZE] = {
-				{1, 2, 3, 4},
-				{3, 0, 1, 2},
-				{3, 2, 1, 4},
-				{4, 6, 4, 7}};
-		return getFilledGrid(values);
-	}
+	MilGrid getFilledGrid();
 
-
-	void writeArbitratyValues(MilGrid& grid, TILE values[SIZE][SIZE])
-	{
-		TILE arbValues[SIZE][SIZE] = {
-				{1, 2, 3, 4},
-				{3, 0, 1, 2},
-				{3, 2, 1, 4},
-				{4, 6, 4, 7}};
-
-		runOnTiles([&](tilePtr c, tilePtr r)
-		{
-			grid[c][r] = arbValues[c][r];
-			values[c][r] = arbValues[c][r];
-		});
-	}
-
+	void writeArbitratyValues(MilGrid& grid, TILE values[SIZE][SIZE]);
+	
 	template <typename T>
-	void matrixAreEqual(T expected[][SIZE], T value[][SIZE], size_t size)
-	{
-		runOnTiles([&]() { Assert::AreEqual(expected[c][r], value[c][r]); });
-	}
+	void matrixAreEqual(T expected[][SIZE], T value[][SIZE], size_t size);
 }
