090b9e70f43a8b4e0352349cf0bb45ad8e46994b
buccia85@gmail.com <buccia85@gmail.com@94d6117c-1b27-d5c4-1503-c26876d1a635>
2014-07-28 20:46:16 +0000

    Migrated QueryTest.cpp

--- trunk/2048.Core.Tests/QueryTest.cpp
+++ trunk/2048.Core.Tests/QueryTest.cpp
@@ -1,161 +1,117 @@
+#pragma once
+
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include "2048Core.h"
+#include "TestUtils.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace My2048CoreTests
+namespace MilGridTests
 {
-	using namespace Core2048;
+	using namespace MilCore;
 
 	TEST_CLASS(QueryTest)
 	{
 	public:
 
-		TEST_METHOD(LineIsEmptyTest)
-		{
-			TILE l1[SIZE] = {1, 2, 3, 4};
-			Assert::IsFalse(IsEmpty(l1));
-
-			TILE l2[SIZE] = {0, 1, 0, 0};
-			Assert::IsFalse(IsEmpty(l2));
-			
-			TILE l3[SIZE] = {0, 0, 0, 0};
-			Assert::IsTrue(IsEmpty(l3));
-		}
-
-		TEST_METHOD(LineIsEmptyCountTest)
-		{
-			TILE l1[SIZE] = {1, 2, 3, 0};
-			Assert::IsFalse(IsEmpty(l1, 3));
-
-			TILE l2[SIZE] = {0, 0, 1, 0};
-			Assert::IsFalse(IsEmpty(l2, 3));
-			Assert::IsTrue(IsEmpty(l2, 2));
-		}
-
-		TEST_METHOD(LineCanMoveTest)
-		{
-			TILE l1[SIZE] = {1, 2, 3, 4};
-			Assert::IsFalse(CanMove(l1));
-
-			TILE l2[SIZE] = {0, 0, 1, 0};
-			Assert::IsTrue(CanMove(l2));
-
-			TILE l3[SIZE] = {0, 0, 0, 0};
-			Assert::IsFalse(CanMove(l3));
-
-			TILE l4[SIZE] = {0, 0, 1, 1};
-			Assert::IsTrue(CanMove(l4));
-
-			TILE l5[SIZE] = {0, 1, 1, 0};
-			Assert::IsTrue(CanMove(l5));
-
-			TILE l6[SIZE] = {2, 2, 3, 4};
-			Assert::IsTrue(CanMove(l6));
-
-			TILE l7[SIZE] = {1, 0, 0, 0};
-			Assert::IsTrue(CanMove(l7));
-		}
-
-		TEST_METHOD(CanMoveGridAndTokenTest1)
+		TEST_METHOD(CanMoveGridTest1)
 		{
 			TILE grid[SIZE][SIZE] = {{1, 2, 3, 4},
 			                         {0, 3, 2, 0},
 			                         {9, 2, 3, 4},
 			                         {9, 8, 4, 2}};
-			AssertCanMove(grid,
+			assertCanMove(grid,
 				/*    UP */ true,
 				/*  DOWN */ true,
 				/*  LEFT */ true,
 				/* RIGHT */ true);
 		}
 
-		TEST_METHOD(CanMoveGridAndTokenTest2)
+		TEST_METHOD(CanMoveGridTest2)
 		{
 			TILE grid[SIZE][SIZE] = {{1, 2, 3, 4},
 			                         {0, 3, 2, 1},
 			                         {9, 2, 3, 4},
 			                         {9, 8, 4, 2}};
-			AssertCanMove(grid,
+			assertCanMove(grid,
 				/*    UP */ true,
 				/*  DOWN */ true,
 				/*  LEFT */ true,
 				/* RIGHT */ false);
 		}
 
-		TEST_METHOD(CanMoveGridAndTokenTest3)
+		TEST_METHOD(CanMoveGridTest3)
 		{
 			TILE grid[SIZE][SIZE] = {{1, 2, 3, 4},
 			                         {2, 3, 2, 1},
 			                         {9, 2, 3, 4},
 			                         {9, 8, 4, 2}};
-			AssertCanMove(grid,
+			assertCanMove(grid,
 				/*    UP */ true,
 				/*  DOWN */ true,
 				/*  LEFT */ false,
 				/* RIGHT */ false);
 		}
 
-		TEST_METHOD(CanMoveGridAndTokenTest5)
+		TEST_METHOD(CanMoveGridTest5)
 		{
 			TILE grid[SIZE][SIZE] = {{1, 2, 3, 4},
 			                         {2, 3, 2, 1},
 			                         {7, 2, 3, 4},
 			                         {9, 8, 4, 2}};
-			AssertCanMove(grid,
+			assertCanMove(grid,
 				/*    UP */ false,
 				/*  DOWN */ false,
 				/*  LEFT */ false,
 				/* RIGHT */ false);
 		}
 
-		TEST_METHOD(CanMoveGridAndTokenTest6)
+		TEST_METHOD(CanMoveGridTest6)
 		{
 			TILE grid[SIZE][SIZE] = {{1, 2, 3, 0},
 			                         {2, 3, 2, 0},
 			                         {7, 2, 3, 0},
 			                         {9, 8, 4, 0}};
-			AssertCanMove(grid,
+			assertCanMove(grid,
 				/*    UP */ false,
 				/*  DOWN */ false,
 				/*  LEFT */ false,
 				/* RIGHT */ true);
 		}
 
-		TEST_METHOD(CanMoveGridAndTokenTest7)
+		TEST_METHOD(CanMoveGridTest7)
 		{
 			TILE grid[SIZE][SIZE] = {{1, 2, 3, 4},
 			                         {2, 3, 2, 1},
 			                         {7, 2, 3, 4},
 			                         {0, 0, 0, 0}};
-			AssertCanMove(grid,
+			assertCanMove(grid,
 				/*    UP */ false,
 				/*  DOWN */ true,
 				/*  LEFT */ false,
 				/* RIGHT */ false);
 		}
 
-		TEST_METHOD(CanMoveGridAndTokenTest8)
+		TEST_METHOD(CanMoveGridTest8)
 		{
 			TILE grid[SIZE][SIZE] = {{0, 0, 0, 0},
 			                         {0, 0, 0, 1},
 			                         {0, 0, 3, 4},
 			                         {0, 8, 4, 2}};
-			AssertCanMove(grid,
+			assertCanMove(grid,
 				/*    UP */ true,
 				/*  DOWN */ false,
 				/*  LEFT */ true,
 				/* RIGHT */ false);
 		}
 
-		TEST_METHOD(CanMoveGridAndTokenTest9)
+		TEST_METHOD(CanMoveGridTest9)
 		{
 			TILE grid[SIZE][SIZE] = {{0, 0, 0, 0},
 			                         {0, 0, 0, 1},
 			                         {0, 0, 3, 4},
 			                         {0, 8, 4, 4}};
-			AssertCanMove(grid,
+			assertCanMove(grid,
 				/*    UP */ true,
 				/*  DOWN */ true,
 				/*  LEFT */ true,
@@ -164,12 +120,13 @@ namespace My2048CoreTests
 
 	private:
 
-		void AssertCanMove(TILE grid[][SIZE], bool canMoveUp, bool canMoveDown, bool canMoveLeft, bool canMoveRight)
+		void assertCanMove(TILE grid[][SIZE], bool canMoveUp, bool canMoveDown, bool canMoveLeft, bool canMoveRight)
 		{
-			Assert::AreEqual(canMoveUp, CanMove(grid, Direction::Up));
-			Assert::AreEqual(canMoveDown, CanMove(grid, Direction::Down));
-			Assert::AreEqual(canMoveLeft, CanMove(grid, Direction::Left));
-			Assert::AreEqual(canMoveRight, CanMove(grid, Direction::Right));
+			auto milgrid = getFilledGrid(grid);
+			Assert::AreEqual(   canMoveUp, milgrid.canMove(Direction::Up));
+			Assert::AreEqual( canMoveDown, milgrid.canMove(Direction::Down));
+			Assert::AreEqual( canMoveLeft, milgrid.canMove(Direction::Left));
+			Assert::AreEqual(canMoveRight, milgrid.canMove(Direction::Right));
 		}
 
 	};
---
+++ trunk/2048.Core.Tests/TestUtils.cpp
@@ -0,0 +1,70 @@
+#include "stdafx.h"
+#include "TestUtils.h"
+
+namespace MilGridTests
+{
+	void runOnTiles(std::function<void (tilePtr c, tilePtr r)> func)
+	{
+		for (tilePtr c=0; c<SIZE; ++c)
+		{
+			for (tilePtr r=0; r<SIZE; ++r)
+			{
+				func(c, r);
+			}
+		}
+	}
+
+	void copy(MilGrid& grid, TILE values[SIZE][SIZE])
+	{
+		runOnTiles([&](tilePtr c, tilePtr r)
+		{
+			grid[c][r] = values[c][r];
+		});
+	}
+
+	MilGrid getEmptyGrid()
+	{
+		MilGrid result;
+		result.resetGrid();
+		return result;
+	}
+
+	MilGrid getFilledGrid(TILE values[SIZE][SIZE])
+	{
+		MilGrid grid;
+		copy(grid, values);
+		return grid;
+	}
+
+	MilGrid getFilledGrid()
+	{
+		TILE values[SIZE][SIZE] = {
+				{1, 2, 3, 4},
+				{3, 0, 1, 2},
+				{3, 2, 1, 4},
+				{4, 6, 4, 7}};
+		return getFilledGrid(values);
+	}
+
+
+	void writeArbitratyValues(MilGrid& grid, TILE values[SIZE][SIZE])
+	{
+		TILE arbValues[SIZE][SIZE] = {
+				{1, 2, 3, 4},
+				{3, 0, 1, 2},
+				{3, 2, 1, 4},
+				{4, 6, 4, 7}};
+
+		runOnTiles([&](tilePtr c, tilePtr r)
+		{
+			grid[c][r] = arbValues[c][r];
+			values[c][r] = arbValues[c][r];
+		});
+	}
+
+	template <typename T>
+	void matrixAreEqual(T expected[][SIZE], T value[][SIZE], size_t size)
+	{
+		runOnTiles([&]() { Assert::AreEqual(expected[c][r], value[c][r]); });
+	}
+}
